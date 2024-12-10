5ea62357385de2c1c622fc2e954267f78108bc7c
Malypar <malykparker55@yahoo.com>
2024-12-09 22:17:35 -0600

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,67 @@
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private int accountID;
+    private double balance;
+
+    public BankAccount(String firstName, String lastName, int accountID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountID = accountID;
+        this.balance = 0.0;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Invalid withdrawal amount");
+        }
+    }   
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void accountSummary() {
+        System.out.println("Account Summery");
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Account ID: " + accountID);
+        System.out.println("Balance: $" + balance);
+    }
+}
