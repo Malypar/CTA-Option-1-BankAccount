51bdfbccac8e188c5827f4226df10b48a9a3ba2a
Malypar <malykparker55@yahoo.com>
2024-12-09 22:20:09 -0600

    changes

---
+++ BankAccount.java
@@ -0,0 +1,77 @@
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private int accountID;
+    private double balance;
+
+    //CONSTRUCTOR
+
+    public BankAccount(String firstName, String lastName, int accountID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountID = accountID;
+        this.balance = 0.0;
+    }
+
+    //DEPOSIT METHOD
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    //WITHDRAW METHOD
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Invalid withdrawal amount");
+        }
+    }   
+
+    //GET AND SET METHODS
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    //ACCOUNT SUMMARY
+
+    public void accountSummary() {
+        System.out.println("Account Summery");
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Account ID: " + accountID);
+        System.out.println("Balance: $" + balance);
+    }
+}
---
+++ CheckingAccount.java
@@ -0,0 +1,17 @@
+public class CheckingAccount extends BankAccount {
+    private double interestRate;
+
+    //CONSTRUCTOR
+
+
+
+
+    //WITHDRAW METHOD WITH $30 OVERDRAFT FEE
+    
+    
+
+    //DISPLAY ACCOUNT DETAILS
+
+
+
+}
